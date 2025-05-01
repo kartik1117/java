f3aa46f3a43a356556d631d9a9e1de7874f0ec1c
kartik1117 <kartikchaturvedi2004@gmal.com>
2025-05-01 13:34:24 +0530

    initial commit

--- String/String2.java
+++
@@ -1,6 +0,0 @@
-class String2 {
-    public static void main(String[] args) {
-        String str = ("hello" + "hii" );
-        System.out.println(str);
-    }
-}
--- first.java
+++
@@ -1,7 +0,0 @@
-class first {
-    public static void main(String[] args) {
-      int a = 69;
-      byte b = (byte) a;
-        System.out.println(b);
-    }
-}
